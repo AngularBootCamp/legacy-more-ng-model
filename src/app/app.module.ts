e1e51231ef21a3ee7306c92e866afdd81851a893
Kyle Cordes <kyle.cordes@oasisdigital.com>
2018-08-08 11:52:25 -0500

    curriculum update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 
-import { ColorComponent }  from './color';
+import { ColorComponent } from './color';
 
 @NgModule({
   declarations: [ColorComponent],
